b99a0ac9e372c6f22869c9685cea6dc6afa6e8ca
Mariana Santos <mariihsantos20@gmail.com>
2025-06-25 16:40:00 -0300

    atualização

--- src/routes/ProductsRoutes.js
+++ src/routes/ProductsRoutes.js
@@ -3,3 +3,13 @@ const ProductsController = require('../controllers/ProductsController');
 const ProductRoutes = express.Router();
 
 const productController = new ProductsController();
+
+
+ProductRoutes.get('/search', (req, res) => productController.search(req, res));
+ProductRoutes.get('/:id', (req, res) => productController.getProductById(req, res));
+ProductRoutes.post('/', (req, res) => productController.create(req, res));
+ProductRoutes.put('/:id', (req, res) => productController.update(req, res));
+ProductRoutes.delete('/:id', (req, res) => productController.delete(req, res));
+
+module.exports = ProductRoutes;
+
