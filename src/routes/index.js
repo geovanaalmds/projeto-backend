90aa00a12ecd7982aa92acc08b2215975ab5acc5
Kacloks <117231721+Kacloks@users.noreply.github.com>
2025-06-26 17:14:54 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,20 @@ const router = express.Router();
 const userRoutes = require('../routes/UsersRoutes');
 const categoryRoutes = require('../routes/CategoriesRoutes');
 const productRoutes = require('../routes/ProductsRoutes');
-//teste da rota
-router.get('/', (request, response) => {
-  response.json({ status: 'API funcionando' });
+const authRoutes = require('../routes/AuthRoutes');
+
+const AuthMiddleware = require('../middlewares/AuthMiddleware');
+
+// Teste de rota
+router.get('/', (req, res) => {
+  res.json({ status: 'API funcionando' });
 });
 
-router.use('/v1/user', userRoutes); //pra usuários
-router.use('/v1/category', categoryRoutes); //pra categorias
+router.use('/v1/user', authRoutes); // Rota de autenticação
+router.use(AuthMiddleware); // Protege rotas abaixo com o middleware
+
+router.use('/v1/user', userRoutes);
+router.use('/v1/category', categoryRoutes);
 router.use('/v1/product', productRoutes);
-
-
 
 module.exports = router;
